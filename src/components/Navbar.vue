1099acb0c03953c73d178b3f6c324b35f5e5109e
marsodev <marceaud.pro@gmail.com>
2025-05-16 00:14:11 +0200

    training on navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,40 @@
 <template>
-  <nav>
-    <ul>
-      <li>Home</li>
-      <li>ToDo</li>
-      <li>ABOUT</li>
+  <nav class="navigation">
+    <ul class="navigation__container">
+      <li class="navigation__item">
+        <a href="#" class="navigation__link">Home</a>
+      </li>
+      <li class="navigation__item">
+        <a href="" class="navigation__link">Home</a>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navigation {
+  background-color: var(--main-color);
+  width: 100vw; // occupe toute la largeur visible du viewport
+  margin-left: calc(-1 * var(--side-padding)); // annule le padding du body
+  margin-right: calc(-1 * var(--side-padding));
+  display: flex;
+  justify-content: center;
+
+  &__container {
+    display: flex;
+    gap: 30px;
+    padding: 10px 0;
+  }
+
+  &__item {
+    background-color: aqua;
+  }
+
+  &__link {
+    text-decoration: none;
+    color: white;
+  }
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -8,4 +8,8 @@
 import Navbar from "@/components/Navbar.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout {
+  width: 100%;
+}
+</style>
