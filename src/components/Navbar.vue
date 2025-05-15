9e1c8813e52d36cae960e9eb1b717a82d64ff17f
marsodev <marceaud.pro@gmail.com>
2025-05-15 20:45:22 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>ToDo</li>
+      <li>ABOUT</li>
+    </ul>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<style lang="scss" scoped></style>
